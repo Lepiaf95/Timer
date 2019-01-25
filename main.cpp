eb5a57d70678dc12f69b70333cd997d4a924d9c3
Geoffrey <45283124+Lepiaf95@users.noreply.github.com>
2019-01-26 00:19:17 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include "Time.h"
-
+#include <thread>
 
 using namespace std;
 
 int main()
 {
     cout << "Hello World!\n"; 
-	Time time(60);
+	thread time1(&Time::increaseTime, Time(10)); // compte jusqu'a 10
+	thread time2(&Time::increaseTime, Time(5)); // compte jusqu'a 10 a partir de time1 = 5
+	cout << "briovnreibvervep" << endl;
+	time1.join();
+	cout << "ieruhpeiurhferi" << endl;
+	time2.join();
 	cout << "Bye Bye World!\n";
 	system("pause");
 	return 0;
