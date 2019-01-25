90b44d237f5f5558e64394bf4737894d15bdc006
Geoffrey <45283124+Lepiaf95@users.noreply.github.com>
2019-01-26 00:21:22 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
 {
     cout << "Hello World!\n"; 
 	thread time1(&Time::increaseTime, Time(30)); // compte jusqu'a 60
-	thread time2(&Time::increaseTime, Time(30)); // compte jusqu'a 10 a partir de time1 = 30
+	thread time2(&Time::increaseTime, Time(30)); // compte jusqu'a 60 a partir de time1 = 30
 	time1.join();
 	time2.join();
 	cout << "Bye Bye World!\n";
